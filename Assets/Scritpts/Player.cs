cb14b9bb7d5d6a0ef9a87641ba1d71fb56e1c163
amika1107 <cc190098@cis.fukuoka-u.ac.jp>
2021-07-28 19:58:12 +0900

    画像が反転してるから動きはおかしいけど、動くようになったよ

---
+++ Assets/Scritpts/Player.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //インスペクターで設定する
+    public float speed;
+
+    //プライベート変数
+    private Animator anim = null;
+    private Rigidbody2D rb = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //コンポーネントのインスタンスを捕まえる
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キー入力されたら行動する
+        float horizontalKey = Input.GetAxis("Horizontal");
+        float xSpeed = 0.0f;
+
+        if (horizontalKey > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            anim.SetBool("Walk", true);
+            xSpeed = speed;
+        }
+        else if (horizontalKey < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            anim.SetBool("Walk", true);
+            xSpeed = -speed;
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+            xSpeed = 0.0f;
+        }
+        rb.velocity = new Vector2(xSpeed, rb.velocity.y);
+    }
+}
